7f3f014f2b89efbba4dd19726b126da5519b152b
ongmk@Work <ongmk.1997@gmail.com>
2023-06-06 22:43:01 +0800

    pts features engineering

--- fpl/src/fpl/pipelines/model_pipeline/preprocessor.py
+++ fpl/src/fpl/pipelines/model_pipeline/preprocessor.py
@@ -1,6 +1,7 @@
 from typing import Any
 import pandas as pd
 import re
+import itertools
 
 
 def combine_data(
@@ -44,26 +45,6 @@ def combine_data(
         right_on=["team", "next_match"],
         how="left",
         suffixes=("", "_opp"),
-    )
-    combined_data["att_total"] = combined_data.att_elo + combined_data.def_elo_opp
-    combined_data["home_att_total"] = (
-        combined_data.home_att_elo + combined_data.away_def_elo_opp
-    )
-    combined_data["away_att_total"] = (
-        combined_data.away_att_elo + combined_data.home_def_elo_opp
-    )
-    combined_data["def_total"] = combined_data.def_elo + combined_data.att_elo_opp
-    combined_data["home_def_total"] = (
-        combined_data.home_def_elo + combined_data.away_att_elo_opp
-    )
-    combined_data["away_def_total"] = (
-        combined_data.away_def_elo + combined_data.home_att_elo_opp
-    )
-    combined_data = combined_data.drop(
-        combined_data.filter(regex="_elo$").columns, axis=1
-    )
-    combined_data = combined_data.drop(
-        combined_data.filter(regex="_opp$").columns, axis=1
     )
     combined_data = combined_data.drop("next_match", axis=1)
 
@@ -173,21 +154,136 @@ def clean_data(team_match_log, elo_data, odds_data, parameters):
     return combined_data
 
 
-def feature_engineering(combined_data: pd.DataFrame) -> pd.DataFrame:
-    combined_data["days_till_next"] = (
-        (combined_data.groupby("team")["date"].shift(-1) - combined_data["date"])
+def agg_home_away_elo(data: pd.DataFrame) -> pd.DataFrame:
+    data["att_total"] = data.att_elo + data.def_elo_opp
+    data["home_att_total"] = data.home_att_elo + data.away_def_elo_opp
+    data["away_att_total"] = data.away_att_elo + data.home_def_elo_opp
+    data["def_total"] = data.def_elo + data.att_elo_opp
+    data["home_def_total"] = data.home_def_elo + data.away_att_elo_opp
+    data["away_def_total"] = data.away_def_elo + data.home_att_elo_opp
+    data = data.drop(data.filter(regex="_elo$").columns, axis=1)
+    data = data.drop(data.filter(regex="_opp$").columns, axis=1)
+    return data
+
+
+def calculate_points(row):
+    if row["gf"] > row["ga"]:
+        return 3
+    elif row["gf"] == row["ga"]:
+        return 1
+    else:
+        return 0
+
+
+def calculate_daily_rank(date_data: pd.DataFrame) -> pd.Series:
+    date_data = date_data.sort_values(["pts_b4_match", "team"], ascending=[False, True])
+    date_data["rank_b4_match"] = date_data.reset_index().index + 1
+    return date_data
+
+
+def features_from_pts(points_data: pd.DataFrame) -> pd.DataFrame:
+    output_data = pd.DataFrame(
+        columns=[
+            "season",
+            "team",
+            "date",
+            "pts_b4_match",
+            "rank_b4_match",
+            "pts_gap_above",
+            "pts_gap_below",
+        ]
+    )
+    for season, season_data in points_data.groupby("season"):
+        team_date = pd.DataFrame(
+            list(
+                itertools.product(
+                    [season], season_data["team"].unique(), season_data["date"].unique()
+                )
+            ),
+            columns=["season", "team", "date"],
+        )
+        season_data["matchday"] = True
+        fill_dates = team_date.merge(
+            season_data, on=["season", "team", "date"], how="left"
+        )
+
+        fill_dates = fill_dates.sort_values("date")
+        fill_dates["pts_b4_match"] = (
+            fill_dates.groupby("team")["pts_b4_match"].ffill().fillna(0)
+        )
+        fill_dates = (
+            fill_dates.groupby("date")
+            .apply(calculate_daily_rank)
+            .reset_index(drop=True)
+        )
+
+        fill_dates = fill_dates.sort_values(["date", "pts_b4_match"], ascending=False)
+        fill_dates["pts_gap_above"] = (
+            fill_dates.groupby("date")["pts_b4_match"].shift(1)
+            - fill_dates["pts_b4_match"]
+        )
+        fill_dates["pts_gap_below"] = fill_dates["pts_b4_match"] - fill_dates.groupby(
+            "date"
+        )["pts_b4_match"].shift(-1)
+        fill_dates["pts_gap_above"] = fill_dates.groupby("date")[
+            "pts_gap_above"
+        ].transform(lambda x: x.fillna(x.mean()))
+        fill_dates["pts_gap_below"] = fill_dates.groupby("date")[
+            "pts_gap_below"
+        ].transform(lambda x: x.fillna(x.mean()))
+        fill_dates = fill_dates.loc[fill_dates["matchday"] == True].drop(
+            "matchday", axis=1
+        )
+        output_data = pd.concat([output_data, fill_dates])
+
+    output_data = output_data.reset_index(drop=True)
+    return output_data
+
+
+def calculate_pts_data(data: pd.DataFrame) -> pd.DataFrame:
+    pts_data = data.copy()
+
+    pts_data["points"] = pts_data.apply(calculate_points, axis=1)
+    pts_data["total_points"] = (pts_data.groupby(["season", "team"])["points"]).shift(1)
+    pts_data["pts_b4_match"] = (
+        pts_data.groupby(["season", "team"])["total_points"].cumsum().fillna(0)
+    )
+    data["days_till_next"] = (
+        (data.groupby("team")["date"].shift(-1) - data["date"])
         .apply(lambda x: x.days)
         .clip(upper=7)
     )
-    combined_data["days_since_last"] = (
-        (combined_data["date"] - combined_data.groupby("team")["date"].shift(1))
+    columns_to_drop = [
+        col
+        for col in pts_data.columns
+        if col not in ["date", "team", "pts_b4_match", "season"]
+    ]
+    pts_data = pts_data.drop(columns=columns_to_drop)
+
+    pts_data = features_from_pts(pts_data)
+
+    return pts_data
+
+
+def feature_engineering(data: pd.DataFrame) -> pd.DataFrame:
+    data = agg_home_away_elo(data)
+    data["days_till_next"] = (
+        (data.groupby("team")["date"].shift(-1) - data["date"])
         .apply(lambda x: x.days)
         .clip(upper=7)
     )
-    combined_data["xg_ma"] = combined_data.groupby("team")["xg"].apply(
+
+    pts_data = calculate_pts_data(data)
+    data = data.merge(pts_data, on=["season", "team", "date"])
+    data["days_since_last"] = (
+        (data["date"] - data.groupby("team")["date"].shift(1))
+        .apply(lambda x: x.days)
+        .clip(upper=7)
+    )
+    data["xg_ma"] = data.groupby("team")["xg"].apply(
         lambda x: x.shift(1).rolling(window=5).mean()
     )
-    combined_data["xga_ma"] = combined_data.groupby("team")["xga"].apply(
+    data["xga_ma"] = data.groupby("team")["xga"].apply(
         lambda x: x.shift(1).rolling(window=5).mean()
     )
-    return combined_data
+    return data
